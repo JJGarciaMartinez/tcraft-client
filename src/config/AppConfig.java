072ced4a1c4b5eb56c690f734634d0f968b0e398
JJGarciaMartinez <juangm2302@gmail.com>
2025-12-17 13:51:49 -0600

    version: change to beta version

--- src/config/AppConfig.java
+++ src/config/AppConfig.java
@@ -14,5 +14,5 @@ package config;
  */
 public class AppConfig {
     public static final String URL_MANIFEST = "https://raw.githubusercontent.com/JJGarciaMartinez/tcraft-mods-list/main/modList/mod-list.json";
-    public static final String VERSION_APP = "1.0.0";
+    public static final String VERSION_APP = "b-v1.0.0";
 }
